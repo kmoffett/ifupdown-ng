529122752c409701db8a1552ae825e0f79833506
Kyle Moffett <kyle@moffetthome.net>
2013-01-02 13:33:53 -0800

    Add a license summary to the --version option output
    Signed-off-by: Kyle Moffett <kyle@moffetthome.net>

--- ifupdown_ng/commands/__init__.py
+++ ifupdown_ng/commands/__init__.py
@@ -92,8 +92,22 @@ class CommandHandler(object):
 	__metaclass__ = CommandHandlerType
 
 	APP_VERSION_COPYRIGHT = (
-		'ifupdown-ng version %s\n'
-		'Copyright(C) 2012  Kyle Moffett <kyle@moffetthome.net>\n'
+"""ifupdown-ng version %s
+Copyright(C) 2012  Kyle Moffett <kyle@moffetthome.net>
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of version 2 of the GNU General Public License, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; otherwise you can obtain it here:
+  http://www.gnu.org/licenses/gpl-2.0.txt
+"""
 	) % VERSION
 
 	def __init__(self, command, **kwargs):
