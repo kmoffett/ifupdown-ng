3ea44ed0071061d95078a8c5bd1912f4d8c3b26f
Kyle Moffett <kyle@moffetthome.net>
2013-01-03 00:15:53 -0800

    Create helper files and scripts for pylint-cleanness
    The new run_pylint.sh script automatically runs pylint with the new
    pylintrc file on the Python scripts and source directory.
    
    Signed-off-by: Kyle Moffett <kyle@moffetthome.net>

---
+++ run_pylint.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+cd "$(dirname "$0")"
+
+## First run it on non-module scripts with a disabled module regex
+pylint --rcfile=pylintrc --module-rgx='.*'	\
+	ifupdown-ng				\
+	setup.py
+
+## Now run it on the module directory
+pylint --rcfile=pylintrc			\
+	ifupdown_ng/				\
