8db4f5f4fb5848a4bf4230c61cde9752169b706e
Kyle Moffett <kyle@moffetthome.net>
2013-01-03 00:51:13 -0800

    run_pylint.sh: Allow passing extra options during development
    Occasionally we want to suppress certain diagnostics during development,
    such as the "Missing docstring" message.
    
    To make that convenient, forward extra options received by run_pylint.sh
    along to the various pylint subprocesses.
    
    Signed-off-by: Kyle Moffett <kyle@moffetthome.net>

--- run_pylint.sh
+++ run_pylint.sh
@@ -3,10 +3,10 @@
 cd "$(dirname "$0")"
 
 ## First run it on non-module scripts with a disabled module regex
-pylint --rcfile=pylintrc --module-rgx='.*'	\
+pylint --rcfile=pylintrc --module-rgx='.*' "$@"	\
 	ifupdown-ng				\
 	setup.py
 
 ## Now run it on the module directory
-pylint --rcfile=pylintrc			\
+pylint --rcfile=pylintrc "$@"			\
 	ifupdown_ng/				\
